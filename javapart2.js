876790af8887a368d1fed768d52217a7f2cd4286
arlindakastrati <74983643+arlindakastrati@users.noreply.github.com>
2021-01-08 13:38:53 +0100

    Add files via upload
    Should be reviewed

---
+++ javapart2.js
@@ -0,0 +1,5 @@
+//let result =prompt('Are you sure that you wanna delete this?');
+//console.log(result);
+
+function myFunction(){
+    document.getElementById("Ram").innerHTML="Random-access memory";}
